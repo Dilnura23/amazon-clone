a1fbe03231fdbaab4765bcae7d54408747e5e221
superSimpleDev <dilnura@supersimple.dev>
2025-07-06 14:23:22 +0900

    fix price formating and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,7 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { formatCurrency } from './utils/money.js'; 
+//one dot means current folde
 
 let cartSummary = "";
 
@@ -27,7 +29,7 @@ cartSummary+= `
        ${matchingProduct.name}
       </div>
       <div class="product-price">
-        $${(matchingProduct.priceCents/100).toFixed(2)}
+        $${formatCurrency(matchingProduct.priceCents)}
       </div>
       <div class="product-quantity">
         <span>
@@ -49,7 +51,7 @@ cartSummary+= `
       <div class="delivery-option">
         <input type="radio" checked
           class="delivery-option-input"
-          name="delivery-option-1">
+          name="delivery-option-${matchingProduct.id}">
         <div>
           <div class="delivery-option-date">
             Tuesday, June 21
@@ -62,7 +64,7 @@ cartSummary+= `
       <div class="delivery-option">
         <input type="radio"
           class="delivery-option-input"
-          name="delivery-option-1">
+          name="delivery-option-${matchingProduct.id}">
         <div>
           <div class="delivery-option-date">
             Wednesday, June 15
@@ -75,7 +77,7 @@ cartSummary+= `
       <div class="delivery-option">
         <input type="radio"
           class="delivery-option-input"
-          name="delivery-option-1">
+          name="delivery-option-${matchingProduct.id}">
         <div>
           <div class="delivery-option-date">
             Monday, June 13
@@ -89,81 +91,8 @@ cartSummary+= `
   </div>
 </div>
 
-<div class="cart-item-container">
-  <div class="delivery-date">
-    Delivery date: Wednesday, June 15
-  </div>
-
-  <div class="cart-item-details-grid">
-    <img class="product-image"
-      src="images/products/intermediate-composite-basketball.jpg">
-
-    <div class="cart-item-details">
-      <div class="product-name">
-        Intermediate Size Basketball
-      </div>
-      <div class="product-price">
-        $20.95
-      </div>
-      <div class="product-quantity">
-        <span>
-          Quantity: <span class="quantity-label">1</span>
-        </span>
-        <span class="update-quantity-link link-primary">
-          Update
-        </span>
-        <span class="delete-quantity-link link-primary">
-          Delete
-        </span>
-      </div>
-    </div>
-
-    <div class="delivery-options">
-      <div class="delivery-options-title">
-        Choose a delivery option:
-      </div>
-
-      <div class="delivery-option">
-        <input type="radio" class="delivery-option-input"
-          name="delivery-option-2">
-        <div>
-          <div class="delivery-option-date">
-            Tuesday, June 21
-          </div>
-          <div class="delivery-option-price">
-            FREE Shipping
-          </div>
-        </div>
-      </div>
-      <div class="delivery-option">
-        <input type="radio" checked class="delivery-option-input"
-          name="delivery-option-2">
-        <div>
-          <div class="delivery-option-date">
-            Wednesday, June 15
-          </div>
-          <div class="delivery-option-price">
-            $4.99 - Shipping
-          </div>
-        </div>
-      </div>
-      <div class="delivery-option">
-        <input type="radio" class="delivery-option-input"
-          name="delivery-option-2">
-        <div>
-          <div class="delivery-option-date">
-            Monday, June 13
-          </div>
-          <div class="delivery-option-price">
-            $9.99 - Shipping
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-</div>`
+`
 });
 
-console.log(cartSummary);
+// console.log(cartSummary);
 document.querySelector('.js-order-summary').innerHTML = cartSummary;
