860e45c117422cf904882d8d20210bb40af85d32
superSimpleDev <dilnura@supersimple.dev>
2025-07-06 15:19:05 +0900

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem)=>{
   });
 
 cartSummary+= `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
   <div class="delivery-date">
     Delivery date: Tuesday, June 21
   </div>
@@ -102,6 +102,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
     console.log('delete');
     const productID = link.dataset.productId;
     removeFromCart(productID);
-    
+
+    const container = document.querySelector(`.js-cart-item-container-${productID}`);
+    container.remove();
   });
 })
