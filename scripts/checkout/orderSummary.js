dbbf7f7a08100d98baf8a3bbaed9bd78086b21f9
superSimpleDev <dilnura@supersimple.dev>
2025-07-09 09:55:25 +0900

    move orderSummary into its own file

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
-import {cart, removeFromCart, calculateCartQuantity, updateQuantity, saveQuantity, updateDeliveryOption} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {cart, removeFromCart, calculateCartQuantity, updateQuantity, saveQuantity, updateDeliveryOption} from '../../data/cart.js';
+import {products} from '../../data/products.js';
 import formatCurrency from '../utils/money.js'; 
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import { deliveryOptions } from '../..data/deliveryOptions.js';
+import { deliveryOptions } from '../../data/deliveryOptions.js';
 
 // hello();
 // const today = dayjs();
