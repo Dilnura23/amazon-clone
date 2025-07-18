744cc3bfbf5d311ee11d9d9144ced5b558f8f36a
superSimpleDev <dilnura@supersimple.dev>
2025-07-18 13:49:50 +0900

    convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary} from "./checkout/paymentSummary.js";
 //runs everything this it is oop
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
