ac52116c3a5e1a299eb132cce804a2bd536fb87c
superSimpleDev <dilnura@supersimple.dev>
2025-07-20 19:44:53 +0900

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -77,8 +77,32 @@ class Clothing extends Product {
   }
 }
 
+export let products = []
+
+//Callbacks -called back in the future
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', ()=>{
+    products = JSON.parse(xhr.response).map((productDetails)=>{
+      if (productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      else if (productDetails.type === 'appliance'){
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
+   console.log('load products');
 
 
+   fun();
+  });
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+loadProducts();
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -765,5 +789,7 @@ export const products = [
   }
   return new Product(productDetails);
 });
+
+*/
 
 
